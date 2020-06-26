26d2420758a82519e27579edc1a17aaf5312ec3a
Rajaram240 <>
2020-06-26 09:36:39 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'Counter App';
+  count:number =0;
+
+
+  handleIncrease=()=>{
+    this.count=this.count+1;
+  }
+handleDecrease =()=>{
+  this.count=this.count-1;
+  if(this.count>=0)
+  {
+    this.count=this.count-1;
+  }
+  else{
+    this.count=0;
+  }
+
+}
+handleReset=()=>{
+  this.count=0;
+}
+//handleRest(){
+  //thsi.count=0;
+}
+
